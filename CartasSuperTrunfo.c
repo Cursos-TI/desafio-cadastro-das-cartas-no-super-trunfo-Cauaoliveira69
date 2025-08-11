a9395bd65f716214bf305834eb961d775203db4b
Cauaoliveira69 <cauazinhooo.2007@gmail.com>
2025-08-11 22:16:24 +0000

    agora está com tudo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,24 +2,32 @@
 
 int main() {
     // Cidade 1
-    char nome1[30] = "poa";
-    int populacao1 = 1322845;
-    int pontos1 = 8;
-    float pib1 = 81.56; 
-    float area1 = 495.4;
+    char nome1[30] = "POA";
+    char estado1[3] = "RS";  
+    char codigo1[10] = "A01"; 
+    float populacao1; 
+    float pib1;
+    float area1;
+    int pontos1;
 
     // Cidade 2
-    char nome2[30] = "rj";
-    int populacao2 = 6700000; 
-    int pontos2 = 78;
-    float pib2 = 250;
-    float area2 = 1200;
+    char nome2[30] = "RJ";
+    char estado2[3] = "RIOJ";
+    char codigo2[10] = "B01";
+    float populacao2;
+    float pib2; 
+    float area2;
+    int pontos2;
 
     // Entrada cidade 1
     printf("Digite o nome da primeira cidade: ");
     scanf("%s", nome1);
-    printf("Digite a populacao de %s: ", nome1);
-    scanf("%d", &populacao1);
+    printf("Digite o estado de %s: ", nome1);
+    scanf("%s", estado1);
+    printf("Digite o codigo de %s: ", nome1);
+    scanf("%s", codigo1);
+    printf("Digite a populacao de %s (em milhoes, com decimais): ", nome1);
+    scanf("%f", &populacao1);
     printf("Digite o PIB de %s (em bilhoes): ", nome1);
     scanf("%f", &pib1);
     printf("Digite a area de %s (em km²): ", nome1);
@@ -32,31 +40,44 @@ int main() {
     // Entrada cidade 2
     printf("Digite o nome da segunda cidade: ");
     scanf("%s", nome2);
-    printf("Digite a populacao de %s: ", nome2);
-    scanf("%d", &populacao2);
-    printf("Digite o PIB de %s (em bilhoes): ", nome2);
+    printf("Digite o estado de %s: ", nome2);
+    scanf("%s", estado2);
+    printf("Digite o codigo de %s: ", nome2);
+    scanf("%s", codigo2);
+    printf("Digite a populacao de %s(em milhões): ", nome2);
+    scanf("%f", &populacao2);
+    printf("Digite o PIB de %s(em bilhões): ", nome2);
     scanf("%f", &pib2);
-    printf("Digite a area de %s (em km²): ", nome2);
+    printf("Digite a area de %s(em km2): ", nome2);
     scanf("%f", &area2);
     printf("Digite os pontos turisticos de %s: ", nome2);
     scanf("%d", &pontos2);
 
     printf("\n========= CARTAS =========\n");
-    printf("Cidade 1: %s\n", nome1);
-    printf("População: %d\n", populacao1);
+
+        // Cidade 1
+    printf("Cidade 1:\n");
+    printf("Nome: %s\n", nome1);
+    printf("Estado: %s\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("População: %.2f milhões\n", populacao1);
     printf("PIB: %.2f bilhões\n", pib1);
     printf("Área: %.2f km²\n", area1);
     printf("Pontos turísticos: %d\n", pontos1);
 
-    printf("\nCidade 2: %s\n", nome2);
-    printf("População: %d\n", populacao2);
-    printf("PIB: %.2f bilhões\n", pib2);
-    printf("Área: %.2f km²\n", area2);
-    printf("Pontos turísticos: %d\n", pontos2);
+    // Cidade 2
+    printf("\nCidade 2: \n");
+    printf("Nome: %s \n", nome2);
+    printf("Estado: %s \n", estado2);
+    printf("Codigo: %s \n", codigo2);
+    printf("População: %.2f milhões \n", populacao2);
+    printf("PIB: %.2f bilhões \n", pib2);
+    printf("Área: %.2f km² \n", area2);
+    printf("Pontos turísticos: %d \n", pontos2);
 
-    printf("\n O jogador escolhe qual atributo deseja comparar e decide quem venceu.\n");
+    printf("\nO jogador escolhe qual atributo deseja comparar e decide quem venceu.\n");
 
-    return 0;
-}
+        
 
-
+        return 0;
+    }
